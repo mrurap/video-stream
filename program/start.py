a95f0d078152da9f2a2e7782f326b43c78d6d3eb
mrurap <83493335+mrurap@users.noreply.github.com>
2021-12-16 21:44:56 +0330

    Update start.py

--- program/start.py
+++ program/start.py
@@ -83,7 +83,7 @@ async def start_(client: Client, message: Message):
                 ],
                 [
                     InlineKeyboardButton(
-                        "🌐 Source Code", url="https://t.me/mr_urap"
+                        "🌐 Contanc Us", url="https://t.me/mr_urap"
                     )
                 ],
             ]
